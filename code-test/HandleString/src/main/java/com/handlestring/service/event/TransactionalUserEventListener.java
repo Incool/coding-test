c1edae10dd4e34353ee248aa2ccee850bf7a1c5a
Incool <2247856796@qq.com>
2025-09-26 10:46:22 +0800

    注释掉事务绑定事件以及pom中对jpa的依赖

--- code-test/HandleString/src/main/java/com/handlestring/service/event/TransactionalUserEventListener.java
+++ code-test/HandleString/src/main/java/com/handlestring/service/event/TransactionalUserEventListener.java
@@ -1,8 +1,8 @@
 package com.handlestring.service.event;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
+//import org.springframework.transaction.event.TransactionPhase;
+//import org.springframework.transaction.event.TransactionalEventListener;
 
 /**
  * 事务绑定事件.
@@ -13,12 +13,12 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 public class TransactionalUserEventListener {
 
-    /**
-     * 事务提交后触发.
-     * 其它阶段：BEFORE_COMMIT、AFTER_ROLLBACK、AFTER_COMPLETION
-     */
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void afterUserRegisteredCommit(UserRegisteredEvent event) {
-        System.out.println("事务已提交，用户 [" + event.getUserName() + "] 注册成功，执行后续逻辑");
-    }
+//    /**
+//     * 事务提交后触发.
+//     * 其它阶段：BEFORE_COMMIT、AFTER_ROLLBACK、AFTER_COMPLETION
+//     */
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    public void afterUserRegisteredCommit(UserRegisteredEvent event) {
+//        System.out.println("事务已提交，用户 [" + event.getUserName() + "] 注册成功，执行后续逻辑");
+//    }
 }
